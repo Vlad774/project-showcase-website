be8a326b9aff99733848dd363c2a5fa6feff7b8a
Vlad Vesninsky <111026962+Vlad774@users.noreply.github.com>
2024-11-01 14:48:22 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-// Smooth Scroll for Navigation Links
-document.querySelectorAll('.navbar nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+// Initialize AOS library
+AOS.init({
+    duration: 1000, // animation duration
+    once: true      // animation happens only once
 });
